3a3a5a18ec30b441f6b944d211090e9e2dde136e
Madeleine Madsen <mama0002@stud.kea.dk>
2025-12-19 11:32:28 +0100

    Rettelse af sti

--- app/views/components/_header.php
+++ app/views/components/_header.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="/public/assets/css/mixhtml.css">
-    <link rel="stylesheet" href="/public/assets/css/app.css">
-    <link rel="stylesheet" href="/public/assets/css/popup.css">
+    <link rel="stylesheet" href="/public/assets/css/style.css">
     <script defer src="/public/assets/js/popup.js"></script>
     <title>Document</title>
 </head>
