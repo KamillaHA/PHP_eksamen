408b55c14ade0db481f66aa81ab2d262743010ca
Kamilla Huhnke <kamillahuhnke@me.com>
2025-12-19 11:26:37 +0100

    sti fejl

--- app/views/components/_header.php
+++ app/views/components/_header.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="/css/mixhtml.css">
-    <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="/css/popup.css">
-    <script defer src="/js/popup.js"></script>
+    <link rel="stylesheet" href="/public/assets/css/mixhtml.css">
+    <link rel="stylesheet" href="/public/assets/css/app.css">
+    <link rel="stylesheet" href="/public/assets/css/popup.css">
+    <script defer src="/public/assets/js/popup.js"></script>
     <title>Document</title>
 </head>
 <body class="<?= $body_class ?? '' ?>">
